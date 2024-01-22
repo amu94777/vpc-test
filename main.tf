6cd11282529b47c70b16973dcaf76b59c83e9169
amu94777 <amu94777@gmail.com>
2024-01-22 20:20:43 +0530

    vpc

--- main.tf
+++ main.tf
@@ -5,4 +5,7 @@ module "roboshop" {
     environment = var.environment
     comman_tags = var.common_tags
     rvpc_tags = var.rvpc_tags
+    public_subnets_cidr = var.public_subnets_cidr
+    private_subnets_cidr = var.private_subnets_cidr
+    database_subnets_cidr = var.database_subnets_cidr
 }
