9f4e6039bd4250760af9a8aa56917cebf523c6f7
amu94777 <amu94777@gmail.com>
2024-01-23 19:27:15 +0530

    02=sg

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
 module "roboshop" {
-    source = "../terraform-aws-vpc"
-    #source = "git::https://github.com/amu94777/terraform-aws-vpc.git?ref=master"
-    project_name = var.project_name
-    environment = var.environment
-    comman_tags = var.common_tags
-    vpc_tags = var.vpc_tags
-    public_subnets_cidr = var.public_subnets_cidr
-    private_subnets_cidr = var.private_subnets_cidr
-    database_subnets_cidr = var.database_subnets_cidr
-    is_peering_required = var.is_peering_required
+  source = "../terraform-aws-vpc"
+  project_name = var.project_name
+  environment = var.environment
+  common_tags = var.common_tags
+  vpc_tags = var.vpc_tags
+
+  # public subnet
+  public_subnets_cidr = var.public_subnets_cidr
+
+  # private subnet
+  private_subnets_cidr = var.private_subnets_cidr
+
+  database_subnets_cidr = var.database_subnets_cidr
+
 }
--- variables.tf
+++ variables.tf
@@ -1,40 +1,34 @@
-variable "common_tags" {
-   type = map
-   default = {
-    project = "roboshop"
-    environment = "dev"
-    Terraform = true
-   }
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
 }
-variable "vpc_tags" {
-    type = map
-    default = {}
+variable "common_tags" {
+  default = {
+    Project = "roboshop"
+    Environment = "dev"
+    Terraform = "true"
+  }
 }
 
-variable "vpc_cidr" {
-    default = "10.0.0.0/16"
+variable "vpc_tags" {
+  default = {}
 }
+
 variable "project_name" {
-    default = "roboshop"
+  default = "roboshop"
 }
+
 variable "environment" {
-    default = "dev"
+  default = "dev"
 }
+
 variable "public_subnets_cidr" {
-    type = list
-    default = ["10.0.1.0/24","10.0.2.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
+
 variable "private_subnets_cidr" {
-    type = list
-    default = ["10.0.11.0/24","10.0.12.0/24"]
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 variable "database_subnets_cidr" {
-    type = list
     default = ["10.0.21.0/24","10.0.22.0/24"]
-}
-
-variable "is_peering_required" {
-    type = bool
-    default = true
   
 }
